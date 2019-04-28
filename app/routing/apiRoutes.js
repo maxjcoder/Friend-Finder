723b838d9942ea71779fc66726e48c6df6866ef2
team awesome <maxj@email.arizona.edu>
2019-04-28 15:40:21 -0700

    apiroutes line 9 had '_req' in two places

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,11 +2,11 @@ var friends = require("../data/friends.js");
 
 module.exports = function (app) {
 
-    app.get("/api/friends", function (_req, res) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
 
-    app.post("/api/friends", function (_req, res) {
+    app.post("/api/friends", function (req, res) {
 
         var bestMatch = {
             name: "",
