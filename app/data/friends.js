07bb6c6b079c5a927a54a01bc9d882bffd2d3bf3
team awesome <maxj@email.arizona.edu>
2019-04-27 18:23:09 -0700

    pushed new work and logic

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friends = [
+    {
+        name: "James",
+        photo: "https://pixel.nymag.com/imgs/daily/vulture/2018/09/12/12-james-marsden.w330.h330.jpg"
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+        ]
+    }
+]
